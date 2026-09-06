2a1c6aa7555c6abc83938985c8665d5e4c1afaf1
Thang4869 <vanthang48692109@gmail.com>
2026-09-06 23:42:05 +0700

    feat(prisma): update logging configuration to remove 'query' logs in development

--- src/core/database/prisma.ts
+++ src/core/database/prisma.ts
@@ -10,7 +10,7 @@ export const prisma =
   new PrismaClient({
     log:
       process.env.NODE_ENV === "development"
-        ? ["query", "info", "warn", "error"]
+        ? ["info", "warn", "error"]
         : ["error"],
   });
 

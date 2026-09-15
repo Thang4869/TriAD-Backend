7b7888f00766804c0d993401228e7ccc989d5ccf
Thang4869 <vanthang48692109@gmail.com>
2026-09-15 20:20:25 +0700

    refactor(middleware): remove unused requestScope middleware

--- src/shared/middlewares/request.scope.middleware .ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-import { Container } from "@core/di/container";
-
-export function requestScope(rootContainer: Container) {
-  return (req: Request, _res: Response, next: NextFunction): void => {
-    req.container = rootContainer.createScope();
-    next();
-  };
-}
